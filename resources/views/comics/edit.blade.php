03294e6e9fb9c3fb8d57498adc72569cdcd842e7
DavideGila <davide.gila@gmail.com>
2024-01-09 11:40:55 +0100

    created edit

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title', 'Comic Edit')
+
+@section('content')
+<main class="bg-dark">
+    <section class="container mb-4">
+        <form action="{{route('comics.update', $comic->id)}}" method="POST">
+            @csrf
+            @method('PUT')
+            <input type="text" id="title" name="title" placeholder="Inserisci il titolo" class="form-control">
+            <input type="text" id="description" name="description" placeholder="Inserisci la descrizione" class="form-control">
+            <input type="text" id="price" name="price" placeholder="Inserisci il prezzo" class="form-control">
+            <input type="text" id="type" name="type" placeholder="Inserisci il tipo" class="form-control">
+            <input type="text" id="thumb" name="thumb" placeholder="Inserisci l'immagine" class="form-control">
+            <input type="text" id="sale_date" name="sale_date" placeholder="Inserisci la data" class="form-control">
+            <input type="text" id="series" name="series" placeholder="Inserisci la serie" class="form-control">
+
+            <button type="submit">Invia</button>
+        </form>
+    </section>
+</main>
+@endsection
+

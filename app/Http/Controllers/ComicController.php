7b4bc1fa906f42da172eb2c58b5f98d687a1e8c9
DavideGila <davide.gila@gmail.com>
2024-01-09 17:12:51 +0100

    added with to destroy

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -100,7 +100,7 @@ class ComicController extends Controller
     public function destroy(Comic $comic)
     {
         $comic->delete();
-        return to_route('comics.index');
+        return to_route('comics.index')->with('message', "Il prodotto $comic->title è stato eliminato");;
     }
 }
 

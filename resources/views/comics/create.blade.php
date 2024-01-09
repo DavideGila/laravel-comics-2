924d21ae6a89b243c0514f800f209736e15f5b60
DavideGila <davide.gila@gmail.com>
2024-01-09 14:59:34 +0100

    gave style

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -4,18 +4,18 @@
 
 @section('content')
 <main class="bg-dark">
-    <section class="container mb-4">
+    <section class="container py-4">
         <form action="{{route('comics.store')}}" method="POST">
             @csrf
-            <input type="text" id="title" name="title" placeholder="Inserisci il titolo" class="form-control">
-            <input type="text" id="description" name="description" placeholder="Inserisci la descrizione" class="form-control">
-            <input type="text" id="price" name="price" placeholder="Inserisci il prezzo" class="form-control">
-            <input type="text" id="type" name="type" placeholder="Inserisci il tipo" class="form-control">
-            <input type="text" id="thumb" name="thumb" placeholder="Inserisci l'immagine" class="form-control">
-            <input type="text" id="sale_date" name="sale_date" placeholder="Inserisci la data" class="form-control">
-            <input type="text" id="series" name="series" placeholder="Inserisci la serie" class="form-control">
+            <input type="text" id="title" name="title" placeholder="Inserisci il titolo" class="form-control my-3">
+            <input type="text" id="description" name="description" placeholder="Inserisci la descrizione" class="form-control my-3">
+            <input type="text" id="price" name="price" placeholder="Inserisci il prezzo" class="form-control my-3">
+            <input type="text" id="type" name="type" placeholder="Inserisci il tipo" class="form-control my-3">
+            <input type="text" id="thumb" name="thumb" placeholder="Inserisci l'immagine" class="form-control my-3">
+            <input type="text" id="sale_date" name="sale_date" placeholder="Inserisci la data" class="form-control my-3">
+            <input type="text" id="series" name="series" placeholder="Inserisci la serie" class="form-control my-3">
 
-            <button type="submit">Invia</button>
+            <button type="submit" class="btn btn-primary">Invia</button>
         </form>
     </section>
 </main>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -4,19 +4,19 @@
 
 @section('content')
 <main class="bg-dark">
-    <section class="container mb-4">
+    <section class="container py-4">
         <form action="{{route('comics.update', $comic->id)}}" method="POST">
             @csrf
             @method('PUT')
-            <input type="text" id="title" name="title" placeholder="Inserisci il titolo" class="form-control" value="{{old('title', $comic->title)}}">
-            <input type="text" id="description" name="description" placeholder="Inserisci la descrizione" class="form-control" value="{{old('description', $comic->description)}}">
-            <input type="text" id="price" name="price" placeholder="Inserisci il prezzo" class="form-control" value="{{old('price', $comic->price)}}">
-            <input type="text" id="type" name="type" placeholder="Inserisci il tipo" class="form-control" value="{{old('type', $comic->type)}}">
-            <input type="text" id="thumb" name="thumb" placeholder="Inserisci l'immagine" class="form-control" value="{{old('thumb', $comic->thumb)}}">
-            <input type="text" id="sale_date" name="sale_date" placeholder="Inserisci la data" class="form-control" value="{{old('sale_date', $comic->sale_date)}}">
-            <input type="text" id="series" name="series" placeholder="Inserisci la serie" class="form-control" value="{{old('series', $comic->series)}}">
+            <input type="text" id="title" name="title" placeholder="Inserisci il titolo" class="form-control my-3" value="{{old('title', $comic->title)}}">
+            <input type="text" id="description" name="description" placeholder="Inserisci la descrizione" class="form-control my-3" value="{{old('description', $comic->description)}}">
+            <input type="text" id="price" name="price" placeholder="Inserisci il prezzo" class="form-control my-3" value="{{old('price', $comic->price)}}">
+            <input type="text" id="type" name="type" placeholder="Inserisci il tipo" class="form-control my-3" value="{{old('type', $comic->type)}}">
+            <input type="text" id="thumb" name="thumb" placeholder="Inserisci l'immagine" class="form-control my-3" value="{{old('thumb', $comic->thumb)}}">
+            <input type="text" id="sale_date" name="sale_date" placeholder="Inserisci la data" class="form-control my-3" value="{{old('sale_date', $comic->sale_date)}}">
+            <input type="text" id="series" name="series" placeholder="Inserisci la serie" class="form-control my-3" value="{{old('series', $comic->series)}}">
 
-            <button type="submit">Invia</button>
+            <button type="submit" class="btn btn-primary">Invia</button>
         </form>
     </section>
 </main>

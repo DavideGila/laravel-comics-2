fec291d0e32b58b46fb6ea5312737113ee1a8a39
DavideGila <davide.gila@gmail.com>
2024-01-09 18:15:27 +0100

    added an if that shows if there are errors when creating a new card

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,21 +3,36 @@
 @section('title', 'Comic Create')
 
 @section('content')
-<main class="bg-dark">
-    <section class="container py-4">
-        <form action="{{route('comics.store')}}" method="POST">
-            @csrf
-            <input type="text" id="title" name="title" placeholder="Inserisci il titolo" class="form-control my-3">
-            <input type="text" id="description" name="description" placeholder="Inserisci la descrizione" class="form-control my-3">
-            <input type="text" id="price" name="price" placeholder="Inserisci il prezzo" class="form-control my-3">
-            <input type="text" id="type" name="type" placeholder="Inserisci il tipo" class="form-control my-3">
-            <input type="text" id="thumb" name="thumb" placeholder="Inserisci l'immagine" class="form-control my-3">
-            <input type="text" id="sale_date" name="sale_date" placeholder="Inserisci la data" class="form-control my-3">
-            <input type="text" id="series" name="series" placeholder="Inserisci la serie" class="form-control my-3">
+    <main class="bg-dark">
+        <section class="container py-4">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form action="{{ route('comics.store') }}" method="POST">
+                @csrf
+                <input type="text" id="title" name="title" value="{{ old('title') }}" placeholder="Inserisci il titolo"
+                    class="form-control my-3 @error('title') is-invalid @enderror">
+                <input type="text" id="description" name="description" placeholder="Inserisci la descrizione"
+                    class="form-control my-3">
+                <input type="text" id="price" name="price" placeholder="Inserisci il prezzo"
+                    class="form-control my-3">
+                <input type="text" id="type" name="type" placeholder="Inserisci il tipo"
+                    class="form-control my-3">
+                <input type="text" id="thumb" name="thumb" placeholder="Inserisci l'immagine"
+                    class="form-control my-3">
+                <input type="text" id="sale_date" name="sale_date" placeholder="Inserisci la data"
+                    class="form-control my-3">
+                <input type="text" id="series" name="series" placeholder="Inserisci la serie"
+                    class="form-control my-3">
 
-            <button type="submit" class="btn btn-primary">Invia</button>
-        </form>
-    </section>
-</main>
+                <button type="submit" class="btn btn-primary">Invia</button>
+            </form>
+        </section>
+    </main>
 @endsection
-

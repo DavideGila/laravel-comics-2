32385144e8b4217253330025bc2b46c55edc0404
DavideGila <davide.gila@gmail.com>
2024-01-10 14:40:42 +0100

    created UpdateComicRequest

---
+++ app/Http/Requests/UpdateComicRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'description' => '|required|min:5|max:255',
+            'thumb' => 'url',
+            'price'=> '|required|min:5|max:255',
+            'title'=> '|required|min:5|max:255',
+            'sale_date'=> '|required|',
+            'series'=> '|required|min:5|max:255',
+            'type'=> '|required|min:5|max:255'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'description.min' => 'Il campo descrizione deve avere :min caratteri',
+            'thumb.required' => 'Il campo immagine è obbligatorio',
+            'price.required' => 'Il campo prezzo è obbligatorio',
+            'title.required' => 'Il campo titolo è obbligatorio',
+            'sale_date.required' => 'Il campo data è obbligatorio',
+            'series.max' => 'Il campo serie non può superare i :max caratteri',
+            'type.max' => 'Il campo tipo non può superare i :max caratteri'
+        ];
+    }
+}
